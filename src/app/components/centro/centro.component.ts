6c5baeaf52ad8f0d8bb9b1920b51e170a244cc1f
Jaime Aparicio <astronus@gmail.com>
2022-11-25 14:08:43 +0100

    Centro component

--- src/app/components/centro/centro.component.ts
+++ src/app/components/centro/centro.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CentersService } from '../../services/centers.service';
 
 @Component({
   selector: 'app-centro',
   templateUrl: './centro.component.html',
   styleUrls: ['./centro.component.css']
 })
-export class CentroComponent {
+export class CentroComponent implements OnInit{
+  
+  constructor(
+    private centroService: CentersService
+  ) {}
 
+  ngOnInit(): void {
+      this.centroService.getCenters();
+  }
 }
